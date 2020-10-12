74a06df3359096b8dab67a6ffcadb700c75022a5
zuomiaoshun <zuomiaoshun@pcuu.com>
2020-10-12 11:55:13 +0800

    vue响应式

---
+++ src/33_Vue2.0响应式/index.js
@@ -0,0 +1,59 @@
+const methods = ['push', 'pop', 'shift', 'unshift']
+const arrayProto = Array.prototype
+const arrayMethods = Object.create(arrayProto)
+
+methods.forEach(key => {
+  arrayMethods[key] = function () {
+    const originMethod = arrayProto[key]
+    const result = originMethod.call(this, ...arguments)
+    // observe(inserted)
+    // dep.notify()
+    return result
+  }
+})
+
+function defineReactive (data, key, value) {
+  observe(value)
+  // const dep = new Dep()
+  Object.defineProperty(data, key, {
+    get () {
+      // dep.depend
+      return value
+    },
+    set (newVal) {
+      if (value != newVal) {
+        console.log(value, newVal)
+        value = newVal
+        observe(newVal)
+        // dep.notify
+      }
+    },
+    enumerable: true,
+    configurable: true
+  })
+}
+
+function observe (data) {
+
+  if (typeof data !== 'object' || data == null) return
+
+  if (Array.isArray(data)) {
+    Object.setPrototypeOf(data, arrayMethods)
+  }
+
+  Object.keys(data).forEach(key => {
+    defineReactive(data, key, data[key])
+  })
+}
+
+
+const obj = {
+  name: '斗图王',
+  car: {
+    brand: '兰博基尼'
+  }
+}
+
+observe(obj)
+obj.name = '请叫我斗图王'
+obj.car.brand = '法拉利'
---
+++ src/34_Vue3.0响应式/index.js
@@ -0,0 +1,94 @@
+function isObject (obj) {
+  return typeof obj === 'object' && obj != null
+}
+
+function reactive (obj) {
+  if (!isObject(obj)) return
+
+  const observed = new Proxy(obj, {
+    get (target, key, receiver) {
+      const ret = Reflect.get(target, key, receiver)
+      console.log('get-->', target, key)
+      track(target, key)
+      return isObject(ret) ? reactive(ret) : ret
+    },
+    set (target, key, value, receiver) {
+      const ret = Reflect.set(target, key, value, receiver)
+      console.log('set->', target, key, value)
+      trigger(target, key)
+      return ret
+    },
+    deleteProperty (target, key) {
+      const ret = Reflect.defineProperty(target, key)
+      console.log('deleteProperty->', target, key)
+      trigger(target, key)
+      return ret
+    }
+  })
+
+  return observed
+}
+
+const effectStack = []
+
+function effect (callback) {
+  const callbackWrapper = function () {
+    try {
+      effectStack.push(callbackWrapper)
+      return callback()
+    } finally {
+      effectStack.pop()
+    }
+  }
+  callbackWrapper()
+  return callbackWrapper
+}
+
+const targetMap = new WeakMap()
+
+function track (target, key) {
+  const effectFn = effectStack[effectStack.length - 1]
+  if (effectFn) {
+    let depsMap = targetMap.get(target)
+    if (!depsMap) {
+      depsMap = new Map()
+      targetMap.set(target, depsMap)
+    }
+    let deps = depsMap.get(key)
+    if (!deps) {
+      deps = new Set()
+      depsMap.set(key, deps)
+    }
+    deps.add(effectFn)
+  }
+}
+
+function trigger (target, key) {
+  const depsMap = targetMap.get(target)
+  if (depsMap) {
+    const deps = depsMap.get(key)
+    if (deps) {
+      deps.forEach(effect => effect())
+    }
+  }
+}
+
+const obj = {
+  name: '斗图王',
+  car: {
+    brand: '兰博基尼'
+  }
+}
+
+const state = reactive(obj)
+
+function updateDom () {
+  document.getElementById('app').innerHTML = `${state.name} --> ${state.car.brand}`
+}
+
+effect(updateDom)
+
+setTimeout(() => {
+  state.name = '请叫我斗图王'
+  state.car.brand = '法拉利'
+}, 3000)
